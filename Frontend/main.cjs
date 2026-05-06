07d72eb061ce3e62bea30dce18e6eff4c320dc7c
Joakim Kiugi <kiugijoakim@gmail.com>
2026-05-06 21:47:36 +0300

    Initialize Electron app with main window setup

--- Frontend/main.cjs
+++ Frontend/main.cjs
@@ -1,1 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
 
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // This loads your React app into the Electron window
+  win.loadURL(
+    isDev
+      ? 'http://localhost:5173'
+      : `file://${path.join(__dirname, '../dist/index.html')}`
+  );
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});

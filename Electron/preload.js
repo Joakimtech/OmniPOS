62d19ead4c249339fce5b23d14fba4d94a55ea16
Joakim Kiugi <kiugijoakim@gmail.com>
2026-05-09 23:39:47 +0300

    Add contextBridge API exposure in preload.js
    Expose methods for sending notifications and getting app version.

--- Electron/preload.js
+++ Electron/preload.js
@@ -1,1 +1,11 @@
+const { contextBridge, ipcRenderer } = require('electron');
 
+// Expose protected methods that allow the renderer process (React) to use
+// specific electron APIs without exposing the entire object
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Example: Send a message to the main process
+  sendNotification: (message) => ipcRenderer.send('notify', message),
+  
+  // Example: Get the app version
+  getAppVersion: () => ipcRenderer.invoke('get-version')
+});
